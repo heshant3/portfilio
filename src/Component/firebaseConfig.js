a40c5497bec8d8f9f9bb21939f463ea88b9df677
Heshan Tharindu Kalubowila <55088720+heshant3@users.noreply.github.com>
2025-11-19 19:25:12 +0530

    up

--- src/Component/firebaseConfig.js
+++ src/Component/firebaseConfig.js
@@ -1,17 +1,1 @@
-// firebaseConfig.js
 
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDczOInv5GzD8oDLFRETEJ7IQLzo6eZMBQ",
-  authDomain: "portfolio-154b4.firebaseapp.com",
-  projectId: "portfolio-154b4",
-  storageBucket: "portfolio-154b4.appspot.com",
-  messagingSenderId: "282509340517",
-  appId: "1:282509340517:web:6a37a619bf7a5e0c8ffb8c",
-  measurementId: "G-S0HYKMDPLX",
-};
-
-const app = initializeApp(firebaseConfig);
-
-export default app;
